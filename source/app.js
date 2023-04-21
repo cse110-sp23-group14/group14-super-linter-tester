e25e36c96db9b6e770bbd1c81a823f950d41db95
Nicholas Nakano <nicksnak01@gmail.com>
2023-04-20 19:34:30 -0700

    Added comments/documentation to the ChatGPT code. Minor refactoring.

--- source/app.js
+++ source/app.js
@@ -1,3 +1,4 @@
+// Generated text responses that users will see when asking the 8-ball. Randomly selected.
 const answers = [
   "It is certain.",
   "It is decidedly so.",
@@ -20,13 +21,24 @@ const answers = [
   "Very doubtful."
 ];
 
+// 8-Ball image
 const eightBall = document.querySelector(".eight-ball");
+
+// The text output of the 8-ball
 const answerDiv = document.querySelector(".answer");
+
+// Shake Button
 const shakeButton = document.querySelector(".shake-button");
 
-function shake() {
+// Returns a random response as a string from a list of pre-generated answers.
+function getAnswer() {
   const answer = answers[Math.floor(Math.random() * answers.length)];
-  answerDiv.textContent = answer;
+  return answer;
+}
+
+// Grabs a random response for the text output.
+function shake() {
+  answerDiv.textContent = getAnswer();
 }
 
 shakeButton.addEventListener("click", shake);
