db1ab350c7a3eb3ef2a46d170f2606e99e4dd12a
wengthree1cm <chl135@ucsd.edu>
2023-05-09 22:33:03 -0700

    des

--- source/8ball/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", e=>{
-    e.waitUntil(
-        caches.open("static").then(cache =>{
-            return cache.addAll(["./","./8ball-styles.css","./assets/images/icons/icon192.png"]);
-        })
-    );
-});
-
