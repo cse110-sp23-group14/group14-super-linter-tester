dc46e6aeeec11a479ef8c9c5f1a05d4704daebb1
QijunHuMary <huqijunmary@gmail.com>
2023-05-14 13:41:20 -0700

    update title box

--- source/home-script.js
+++ source/home-script.js
@@ -1,1 +1,6 @@
 //store name+bday in local storage for now
+
+let set_btn = document.getElementById("settings-button");
+set_btn.addEventListener("click", function() {
+    location.href = "settings.html"
+})
