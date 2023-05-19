892af26ee99d25e9a827dd8410a83f012ccf0206
nnakano <nnakano@ucsd.edu>
2023-05-19 13:49:40 -0700

    Added code skeleton for horoscope functions

---
+++ source/horoscope.js
@@ -0,0 +1,17 @@
+// Horoscope Generator
+
+/**
+ * Reads a user object and a dailyConditions object and returns an appropriate horoscope.
+ * @returns {String} A horoscope in string form
+ */
+function generateHoroscope(user) {
+    return "I love astrology!";
+}
+
+/**
+ * Retrieves information about the conditions of the day and returns them as an object.
+ * @returns {DailyConditions} The daily conditions as an object
+ */
+function getDailyConditions() {
+
+}
