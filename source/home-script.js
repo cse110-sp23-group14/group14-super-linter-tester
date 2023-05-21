6306e62d62b68fa4e94e0bf5335af094b2b37350
Melody Gill <melodywgill@gmail.com>
2023-05-21 14:46:42 -0700

    Add horoscope popup

--- source/home-script.js
+++ source/home-script.js
@@ -1,6 +1,8 @@
 //store name+bday in local storage for now
 
-let set_btn = document.getElementById("settings-button");
-set_btn.addEventListener("click", function() {
-    location.href = "settings.html"
-})
+// let set_btn = document.getElementById("settings-button");
+// set_btn.addEventListener("click", function() {
+//     location.href = "settings.html";
+//     console.log("switch to settings");
+// })
+
