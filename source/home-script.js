ea6bdc2de42f3fda3cb89b39bea85a6367139874
wengthree1cm <chl135@ucsd.edu>
2023-05-22 16:04:30 -0700

    modified settings, add buttons, add coresponding js functions

--- source/home-script.js
+++ source/home-script.js
@@ -6,6 +6,12 @@ if (localStorage.getItem('name') === null) {
     document.getElementById('name-info').innerHTML = `The name we have for you is ${localStorage.getItem('name')}.`;
 }
 
+
+if (localStorage.getItem('birthday') === null) {
+    document.getElementById('birthday-info').innerHTML = "We don't have your birthday yet.";
+} else {
+    document.getElementById('birthday-info').innerHTML = `The birthday we have for you is ${localStorage.getItem('birthday')}.`;
+}
 /**
  * Saves the string that the user inputs into local storage.
  */
@@ -13,3 +19,21 @@ function saveUserName() {
     var userName = document.getElementById('name').value;
     localStorage.setItem('name', userName);
 }
+function saveBirthday() {
+    var Birthday = document.getElementById('birthday').value;
+    localStorage.setItem('birthday', Birthday);
+}
+
+function clearName() {
+    localStorage.removeItem('name');
+    location.reload(); // Refresh the page
+}
+const clearNameButton = document.getElementById('clear-name-button');
+clearNameButton.addEventListener('click', clearName);
+
+function clearBirthday() {
+    localStorage.removeItem('birthday');
+    location.reload(); // Refresh the page
+}
+const clearBirthdayButton = document.getElementById('clear-birthday-button');
+clearBirthdayButton.addEventListener('click', clearBirthday);
