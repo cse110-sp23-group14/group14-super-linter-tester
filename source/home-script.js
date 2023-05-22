a1baeb408077a3ee832ac1553e7b7634614d6c91
Melody Gill <melodywgill@gmail.com>
2023-05-21 19:27:53 -0700

    Add jsdoc comment

--- source/home-script.js
+++ source/home-script.js
@@ -6,6 +6,9 @@ if (localStorage.getItem('name') === null) {
     document.getElementById('name-info').innerHTML = `The name we have for you is ${localStorage.getItem('name')}.`;
 }
 
+/**
+ * Saves the string that the user inputs into local storage.
+ */
 function saveUserName() {
     var userName = document.getElementById('name').value;
     localStorage.setItem('name', userName);
