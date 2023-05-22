3228b2feb7e4f050e32700185ba1206458267e61
wengthree1cm <chl135@ucsd.edu>
2023-05-22 16:09:42 -0700

    added JSDoc comment for js files

--- source/home-script.js
+++ source/home-script.js
@@ -1,29 +1,41 @@
-//store name+bday in local storage for now
-
+/**
+ * Retrieves the stored name from local storage and displays it on the page.
+ */
 if (localStorage.getItem('name') === null) {
     document.getElementById('name-info').innerHTML = "We don't have your name yet.";
 } else {
     document.getElementById('name-info').innerHTML = `The name we have for you is ${localStorage.getItem('name')}.`;
 }
 
-
+/**
+ * Retrieves the stored birthday from local storage and displays it on the page.
+ */
 if (localStorage.getItem('birthday') === null) {
     document.getElementById('birthday-info').innerHTML = "We don't have your birthday yet.";
 } else {
     document.getElementById('birthday-info').innerHTML = `The birthday we have for you is ${localStorage.getItem('birthday')}.`;
 }
+
 /**
- * Saves the string that the user inputs into local storage.
+ * Saves the string entered by the user as their name into local storage.
  */
 function saveUserName() {
     var userName = document.getElementById('name').value;
     localStorage.setItem('name', userName);
 }
+
+/**
+ * Saves the string entered by the user as their birthday into local storage.
+ */
 function saveBirthday() {
     var Birthday = document.getElementById('birthday').value;
     localStorage.setItem('birthday', Birthday);
 }
 
+
+/**
+ * Clears the stored name from local storage and refreshes the page.
+ */
 function clearName() {
     localStorage.removeItem('name');
     location.reload(); // Refresh the page
@@ -31,6 +43,10 @@ function clearName() {
 const clearNameButton = document.getElementById('clear-name-button');
 clearNameButton.addEventListener('click', clearName);
 
+
+/**
+ * Clears the stored birthday from local storage and refreshes the page.
+ */
 function clearBirthday() {
     localStorage.removeItem('birthday');
     location.reload(); // Refresh the page
--- source/serviceworker.js
+++ source/serviceworker.js
@@ -1,5 +1,14 @@
+/**
+ * The name of the static cache used for the Progressive Web App (PWA).
+ * @type {string}
+ */
 var staticCacheName = "pwa";
- 
+
+/**
+ * Event listener for the 'install' event.
+ * It is triggered when the service worker is being installed.
+ * @param {Event} event - The 'install' event object.
+ */
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
@@ -7,10 +16,15 @@ self.addEventListener("install", function (event) {
     })
   );
 });
- 
+
+/**
+ * Event listener for the 'fetch' event.
+ * It is triggered when a resource is being fetched by the page.
+ * @param {FetchEvent} event - The 'fetch' event object.
+ */
 self.addEventListener("fetch", function (event) {
   console.log(event.request.url);
- 
+
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
