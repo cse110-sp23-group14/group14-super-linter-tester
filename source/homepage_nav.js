97f4c3a33a8452e3dc2f2ba29dae1966860a9671
wincy02 <yol050@ucsd.edu>
2023-05-23 16:40:43 -0700

    function added for Daily Report

---
+++ source/homepage_nav.js
@@ -0,0 +1,28 @@
+/**
+ * make the daily reort highlighted
+ * highlight will be removed after the user read it
+ * it will be highlighted again on the next day (if the current date matches the report date)
+ * report will only be highlighted once per day until it is read by the user
+ * use localStorage to indicate if report has been read or not
+ */
+window.addEventListener('DOMContentLoaded',function(){
+    var daily_report=document.querySelector('.report');
+
+    var currentDate = new Date();
+    var currentDateStr=currentDate.toDateString();
+
+    var reportDate=new Date();
+    var reportDatestr=reportDate.toDateString();
+
+    if(currentDateStr===reportDatestr){
+        var isread=this.localStorage.getItem('report_read');
+        if(isread) daily_report.classList.add('hightlight');
+    }
+   
+    daily_report.addEventListener('click',function(){
+        daily_report.classList.remove('hightlight');
+        localStorage.setItem('report_read', true);
+    })
+
+
+})
