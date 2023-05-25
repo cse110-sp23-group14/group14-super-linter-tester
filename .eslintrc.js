ac0192e80bbb7b9e54cedd7b481b9d6ece7af0bd
Mive <yul233@ucsd.edu>
2023-05-24 22:53:15 -0700

    add auto code linter config file

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+// This is a config file for eslint,
+// a plugin deployed in .codeclimate.yml
+
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
+    rules: {
+      // Add custom rules here
+    },
+  };
+  
