e2e122503c4c08e5c47d706f37219d9331199023
wengthree1cm <chl135@ucsd.edu>
2023-05-29 21:13:20 -0700

    wrote some jest test, but I am not sure how to get the functions, errors exist when I run npm test

--- source/zodiac.test.js
+++ source/zodiac.test.js
@@ -1,10 +1,6 @@
-function readJsonData(filePath) {
-  const files = require('fs');
-  const jsonData = files.readFileSync(filePath, 'utf-8');
-  return JSON.parse(jsonData);
-}
 
 const {
+  readJsonData,
   getZodiacSign,
   getCompatibility
 } = require('./zodiac.js');
@@ -14,8 +10,6 @@ jest.mock('fs');
 fs.readFileSync.mockReturnValue('{"ZodiacSigns": [], "Compatibility": {}}');
 
 describe('getZodiacSign', () => {
-  const zodiacData = readJsonData('./zodiac.json');
-  const zodiacSigns = zodiacData.ZodiacSigns;
   
     it('should return the correct zodiac sign for a given month and day within the range', () => {
       const zodiacSign = getZodiacSign(4, 3, zodiacSigns);
@@ -36,5 +30,40 @@ describe('getZodiacSign', () => {
       const zodiacSign = getZodiacSign(2, 15, zodiacSigns);
       expect(zodiacSign).toBeNull();
     });
+});
+
+
+describe('compatible zodiac signs', () => {
+  it('should return "OK" for Aries and Taurus', () => {
+    const compatibility = getCompatibility('Aries', 'Taurus');
+    expect(compatibility).toBe('OK');
   });
+
+  it('should return "OK" for Taurus and Aries', () => {
+    const compatibility = getCompatibility('Taurus', 'Aries');
+    expect(compatibility).toBe('OK');
+  });
+
+  it('should return "POSITIVE" for Taurus and Virgo', () => {
+    const compatibility = getCompatibility('Taurus', 'Virgo');
+    expect(compatibility).toBe('POSITIVE');
+    const compatibility1 = getCompatibility('Virgo', 'Taurus');
+    expect(compatibility1).toBe('POSITIVE');
+  });
+
+  it('should return "POSITIVE" for Pisces and Cancer', () => {
+    const compatibility = getCompatibility('Pisces', 'Cancer');
+    expect(compatibility).toBe('POSITIVE');
+  });
+
+  it('should return "NEGATIVE" for Taurus and Leo', () => {
+    const compatibility = getCompatibility('Taurus', 'Leo');
+    expect(compatibility).toBe('NEGATIVE');
+  });
+
+  it('should return "NEGATIVE" for Taurus and Gemini', () => {
+    const compatibility = getCompatibility('Taurus', 'Gemini');
+    expect(compatibility).toBe('NEGATIVE');
+  });
+});
 
