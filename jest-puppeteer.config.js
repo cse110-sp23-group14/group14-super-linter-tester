72f8aa95859719fb6add7338ceeede89b300ceae
Combobyte <varun.sreedhar1@gmail.com>
2023-06-04 15:40:30 -0700

    changed syntax2

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,10 +1,5 @@
 module.exports = {
     launch: {
         headless: "new"
-    },
-    server: {
-        command: "npm run serve",
-        port: 9000,
-        launchTimeout: 18000
-      }
+    }
 };
