d51f939eac702e430fdde798fd0f5f41dcd33d05
Combobyte <varun.sreedhar1@gmail.com>
2023-06-04 15:09:15 -0700

    got basic puppeteer

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-    launch: { },
-    server: {
-        command: "npm run serve",
-        port: 9000,
-        launchTimeout: 180000
+    launch: {
+        headless: "new"
     }
 };
--- source/jest_test/e2e.test.js
+++ source/jest_test/e2e.test.js
@@ -1,9 +1,13 @@
+const puppeteer = require("puppeteer");
+
 describe('Basic user flow for Website', () => {
     beforeAll(async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto("http://localhost:9000/source");
-        console.log(await page.title());
+        await page.goto('http://localhost:36873/source/');
     });
-  
-  });
+
+    it('Basic test', async () => {
+        expect(20).toBe(20);
+    });
+});
